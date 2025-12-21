d6aa2668afdf825553ede84f7fc56624959deadf
justinedupa2005 <justinedupa2005@gmail.com>
2025-12-21 14:32:58 +0800

    Added shop.html

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,5 +1,11 @@
+const accountIcon = document.getElementById('account-icon');
 const hamburgerIcon = document.getElementById('hamburger-icon');
 const menu = document.getElementById('menu');
+
+accountIcon.addEventListener('click', () => {
+  window.location.href = "../html/sign-up.html";
+});
+
 
 hamburgerIcon.addEventListener('click', () => {
   menu.classList.toggle('active');
